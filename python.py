295a92eb8b1c815fdcc227ef0e884f98b496edda
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-08-01 01:58:22 +0530

    created python.py file

---
+++ python.py
@@ -0,0 +1,11 @@
+
+# Find the largest of three numbers (a, b, c).
+
+
+
+
+
+# Find out whether a number is even or odd.
+# Printing 1 to 10 numbers using for loop.
+# Printing 1 to 10 numbers using while loop.
+# Printing 10 to 1 numbers using for loop.
