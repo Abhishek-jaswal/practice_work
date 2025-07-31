0f283b5aac767499cc2f0e5982afc4dbb548b90c
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-08-01 02:20:54 +0530

    added new question

--- python.py
+++ python.py
@@ -24,5 +24,9 @@ for i in range(1, 11):
     print(i)
 
 # Printing 1 to 10 numbers using while loop.
+i=1
+while i<=10:
+       print(i)
+       i+1
+       
 # Printing 10 to 1 numbers using for loop.
-# Printing 1 to 10 numbers using for loop.
--- script.js
+++ script.js
@@ -33,6 +33,32 @@ for(i = 0 ; i<=10 ; i++){
 }
 
 // Printing 1 to 10 numbers using while loop.
-
+i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
        
 // # Printing 10 to 1 numbers using for loop.
+for(i = 10 ; i>=1 ; i--){
+    console.log(i);
+}
+
+// Printing 10 to 1 numbers using while loop.
+i=10;
+while(i>=1){
+    console.log(i);
+    i--;
+}
+
+// Print 1 to 10, using for loop but if your number is 5, stop the loop and print "TICKETS ARE OVER".
+for(i=1; i<=10 ;i++){
+    if(i==5){
+        console.log("TICKETS ARE OVER");
+    }
+   
+}
+
+// Print 1 to 10 numbers using for loop but if number is 5 print "SKIPPING NUMBER 5".
+// Printing 2 table up to 2 X 10 = 20 with for loop.
+// Printing 2 table up to 2 X 10 = 20 with while loop.
